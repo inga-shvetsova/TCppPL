6b48a56fc4b62740b2c120db86a4da12dac1a113
inga-shvetsova <lskoryh@aol.com>
2024-05-10 23:05:06 -0400

    Update 1.5*.cpp

--- x.3/1.5*.cpp
+++ x.3/1.5*.cpp
@@ -16,7 +16,7 @@ int main(){
   string first_name; // first_name is a variable of type string
   cin >> first_name; // "see-in" - character input
 
-  cout << "Now, please, enetr your age (followed by 'eneter'):\n";
+  cout << "Now, please, enter your age (followed by 'eneter'):\n";
   int age;
   cin >> age;
   // cin >> first_name >> age; // read a string followed by an integer
